a417910eb12b10a23fd661fd839e4f9b413f3477
RafaelTS <rafaeltorress@gmail.com>
2025-06-09 07:12:34 -0300

    adding utils file and finished test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,12 @@
+import { format } from '../support/utils'
+
 describe('Dev Finances', () => {
 
   beforeEach(() => {
     cy.visit('https://devfinance-agilizei.netlify.app/')
   });
 
-   it('Add Entrance', () => {
+  it('Add Entrance', () => {
     cy.get('#transaction .button').click()
     cy.get('#description').type('Payment')
     cy.get('[name=amount]').type(12000)
@@ -23,7 +25,7 @@ describe('Dev Finances', () => {
     cy.get('button').contains('Salvar').click()
 
     cy.get('#data-table tbody tr').should('have.length', 1)
-    
+
   });
 
   it('Remove Entrance and Costs', () => {
@@ -46,19 +48,65 @@ describe('Dev Finances', () => {
     cy.get('td.description')
       .contains(entrance)
       .parent()
-      .find('img[onClick*=remove]')  
-      .click()   
+      .find('img[onClick*=remove]')
+      .click()
 
     cy.get('td.description')
       .contains(cost)
       .siblings()
-      .children('img[onClick*=remove]')  
-      .click()    
-      
-    it('Validate balance with multiple transactions', () => {
-      
-      
-    });
+      .children('img[onClick*=remove]')
+      .click()
 
   });
-})
+
+  it.only('', () => {
+    const entrance = 'Payment'
+    const cost = 'Rent'
+
+    cy.get('#transaction .button').click()
+    cy.get('#description').type(entrance)
+    cy.get('[name=amount]').type(900)
+    cy.get('[type=date]').type('2025-05-05')
+    cy.get('button').contains('Salvar').click()
+
+
+    cy.get('#transaction .button').click()
+    cy.get('#description').type(cost)
+    cy.get('[name=amount]').type(-200)
+    cy.get('[type=date]').type('2025-05-05')
+    cy.get('button').contains('Salvar').click()
+
+    let incomes = 0
+    let expenses = 0
+
+    cy.get('#data-table tbody tr')
+      .each(($el, index, $list) => {
+        
+        cy.get($el).find('td.income, td.expense').invoke('text').then(text => {
+
+          cy.log(text)
+          cy.log(format(text))
+          if (text.includes('-')) {
+            expenses = expenses + format(text)
+          } else {
+            incomes = incomes + format(text)
+          }
+
+          cy.log(`entrada`, incomes)
+          cy.log(`saída`, expenses)
+
+
+        })
+
+      })
+
+    cy.get('#totalDisplay').invoke('text').then(text => {
+      cy.log(`valor total`, format(text))
+      let formattedTotalDisplay = format(text)
+      let expectedTotal = incomes + expenses
+
+      expect(formattedTotalDisplay).to.eq(expectedTotal)
+    })
+
+  });
+});
---
+++ cypress/support/utils.js
@@ -0,0 +1,29 @@
+export const format = (value)  => {
+    let formattedValue
+
+    formattedValue = value.replace(',', '.')
+    formattedValue = Number(formattedValue.split('$')[1].trim())
+
+    formattedValue = String(value).includes('-') ? -formattedValue : formattedValue
+
+    return formattedValue
+}
+
+export const randonNumber = () => {
+    return Math.floor(Math.random() * 101)
+}
+
+export const prepareLocalStorage = (win) => {
+    win.localstorage.setItem('dev.finances:transactions' , JSON.stringify([
+        {
+            description: "Payment",
+            amount: randonNumber() * 100,
+            date: "09/06/2025"
+        },
+        {
+            description: "Cost",
+            amount: - (randonNumber)() * 100,
+            date: "09/06/2025"
+        }
+    ]))
+} 
